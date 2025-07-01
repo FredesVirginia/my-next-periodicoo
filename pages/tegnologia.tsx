c1bb90ad020c9a1b488c9df6eaff49d75546c0c4
Fredes Virginia <William-Champion@outlook.com>
2025-07-01 03:30:06 -0300

    w

---
+++ pages/tegnologia.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function tegnologia() {
+  return (
+ <div className="p-10">
+      <h1 className="text-3xl font-bold">🏅 Sección Tegnologia</h1>
+      <p className="mt-4">Aquí irán las noticias deportivas del día.</p>
+    </div>
+  )
+}
